26c841093b4d6807f9acacfc4c00b7ce8a28d157
Dom37160 <steiner.dominique@free.fr>
2022-08-04 11:33:15 +0200

    Add files via upload

---
+++ Rush00/tvx/perso/tableau.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tableau.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dosteine <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/15 18:47:39 by dosteine          #+#    #+#             */
+/*   Updated: 2022/07/15 19:55:32 by dosteine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include<stdio.h>
+
+int	creer_table(int x, int y)
+{
+	int grille;
+	int	table;
+
+	x = 5;
+	y = 5;
+	for(x = 1; x < table; x++)
+	{
+		grille; x = 1; 
+		for(y = 1; y < table ; y++)
+		{
+			grille; y = 1;
+			printf ("%d", table);
+		}
+	}
+}
+
+int 	main()
+{
+	creer_table (5,5);
+	return (0);
+}
